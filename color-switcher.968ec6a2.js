18721a8a87ab231c19e2f997aea47b0657abe93a
Scumsy <99488401+Scumsy@users.noreply.github.com>
2022-10-24 15:09:36 +0000

    Deploying to gh-pages from @ Scumsy/JS-small-cases-and-templates@3ef2745013cf2aff4b25c66fe8e330ece6bd7bab 🚀

---
+++ color-switcher.968ec6a2.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){(o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3))&&(t.disabled=!0)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=color-switcher.968ec6a2.js.map
